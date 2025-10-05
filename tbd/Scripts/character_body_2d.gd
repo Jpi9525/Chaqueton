0881531df10ab1331b5611863a2fbe8690b13d51
Judacio500 <darkiman18@gmail.com>
2025-10-04 21:34:56 -0600

    Cambio de escena

--- tbd/Scripts/character_body_2d.gd
+++ tbd/Scripts/character_body_2d.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+func _ready():
+	$Camera2D.make_current()
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
--- tbd/Scripts/menu.gd
+++ tbd/Scripts/menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://game.tscn")
+	get_tree().change_scene_to_file("res://Scenes/centro_control.tscn")
 
 
 func _on_quit_pressed() -> void:
